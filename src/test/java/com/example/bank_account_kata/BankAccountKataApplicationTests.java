da5abc26721a7548c711129eeb5d47e0840d2846
Vannasay <vannasay.souvannavong@gmail.com>
2022-09-25 18:50:51 +0200

    start

---
+++ src/test/java/com/example/bank_account_kata/BankAccountKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_account_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
